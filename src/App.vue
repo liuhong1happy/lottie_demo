d07f2d7d0a23658f1b68ea51c25568135dc0b763
Holly Liu <liuhong1.happy@163.com>
2021-06-21 19:56:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <lottie v-if="defaultOptions" :width="400" :height="400" :options="defaultOptions" @animCreated="handleCreatedAnimation"/>
+  </div>
+</template>
+
+<script>
+import Lottie from './components/Lottie.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Lottie
+  },
+  data() {
+    return {
+      defaultOptions: {
+        loop: true,
+        path: '/demo.json'
+      }
+    }
+  },
+  methods: {
+    handleCreatedAnimation: function(anim) {
+      console.log(anim)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Lottie.vue
@@ -0,0 +1,53 @@
+<template>
+    <lottie-player
+    autoplay
+    controls
+    loop
+    mode="normal"
+    src="https://assets5.lottiefiles.com/packages/lf20_8q8dz850.json"
+    ></lottie-player>
+</template>
+
+<script>
+  import "@lottiefiles/lottie-player";
+  export default {
+    props: {
+      options: {
+        type: Object,
+        required: true
+      },
+      height: Number,
+      width: Number,
+    },
+    // components: {
+    //     LottiePlayer
+    // },
+    data () {
+      return {
+        style: {
+            width: '100%',
+            height: '100%'
+        }
+      }
+    },
+    // mounted () {
+    //   this.anim = lottie.loadAnimation({
+    //       container: this.$refs.lavContainer,
+    //       renderer: 'svg',
+    //       loop: true,
+    //       autoplay: true,
+    //       animationData: this.options.animationData,
+    //       path: this.options.path,
+    //     }
+    //   );
+    //   this.anim.play();
+    //   this.anim.addEventListener('complete', (res)=> {
+    //       console.log('complete', res)
+    //   })
+    //   this.anim.addEventListener('enterFrame', ()=> {
+    //     //   console.log('enterFrame', res)
+    //   })
+    //   this.$emit('animCreated', this.anim)
+    // }
+  }
+</script>
